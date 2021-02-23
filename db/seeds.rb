2219e6013c6dbc2837790d419219170b79a2c039
Gaia Cravesana <gcravesana@gmail.com>
2021-02-23 22:46:34 +0000

    added some ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "Lemon")
-Ingredient.create(name: "Ice")
-Ingredient.create(name: "Mint leaves")
-Ingredient.create(name: "Gin")
-Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Light white Rum")
 Ingredient.create(name: "Rum")
+Ingredient.create(name: "Dark rum")
+Ingredient.create(name: "London Dry Gin")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Red wine")
+Ingredient.create(name: "Port wine")
+Ingredient.create(name: "White wine")
+Ingredient.create(name: "Prosecco")
+Ingredient.create(name: "Brut champagne")
+Ingredient.create(name: "Campari bitter")
+Ingredient.create(name: "Aperol")
+Ingredient.create(name: "Blended Scotch Whiskey")
+Ingredient.create(name: "Brandy")
+Ingredient.create(name: "Cherry brandy liqueur")
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Lemon juice")
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Orange juice")
+Ingredient.create(name: "Coke")
 Ingredient.create(name: "Sugar")
 Ingredient.create(name: "Soda")
-Ingredient.create(name: "Red wine")
-Ingredient.create(name: "White wine")
+Ingredient.create(name: "Tonic water")
+Ingredient.create(name: "Sugar syrup")
+Ingredient.create(name: "Beer")
+Ingredient.create(name: "Cider")
+Ingredient.create(name: "Jenever")
+Ingredient.create(name: "Absinthe")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Fresh mint leaves")
 Ingredient.create(name: "Cinnamon")
-Ingredient.create(name: "Whiskey")
-Ingredient.create(name: "Orange juice")
+Ingredient.create(name: "Pepper")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Sugar")
+
+
+
+
+
+
+
+
+
+
 
